c939a88fc8789b611027243d8908e24d1521b292
Theophilus Omorebee <theo4u@ymail.com>
2017-11-12 08:52:11 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { FormControl, Well } from 'react-bootstrap'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      // <div className='App'>
+      //   <header className='App-header'>
+      //     <img src={logo} className='App-logo' alt='logo' />
+      //     <h1 className='App-title'>Welcome to React</h1>
+      //   </header>
+      //   <p className='App-intro'>
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <div style={{width: '400px', margin: 'auto', marginTop: '50px'}}>
+        <Well>
+          <FormControl
+            type='text'
+          // value={this.state.value}
+            placeholder='What to do'
+         // onChange={this.handleChange}
+          />
+        </Well>
       </div>
     )
   }
